2fa0f6a2cae8266cb30ad678948c21a486805919
Nguyễn Thái An <50405868+thaian229@users.noreply.github.com>
2022-12-23 01:44:35 +0700

    add health and dead for Bat

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var knock_back_speed := 125
 @export	var air_friction := 100
 
+@onready var damageable := $Damageable as Damageable
+
 
 func _physics_process(delta: float) -> void:
 	self.velocity = self.velocity.move_toward(Vector2.ZERO, air_friction * delta)
@@ -14,3 +16,11 @@ func _on_hurtbox_area_entered(area: Area2D):
 	var parent := area.get_parent() as Node2D
 	var direction = (self.global_position - parent.global_position).normalized()
 	self.velocity = direction * knock_back_speed
+	
+	# lose health
+	if damageable:
+		damageable.current_health -= 1
+
+
+func _on_damageable_zero_health():
+	self.call_deferred("queue_free")
---
+++ scripts/shared/damageable.gd
@@ -0,0 +1,14 @@
+class_name Damageable
+extends Node
+
+signal zero_health()
+
+@export var max_health := 1
+
+@onready var current_health := max_health :
+	get: return current_health
+	set(v):
+		current_health = v
+		current_health = clampi(current_health, 0, max_health)
+		if current_health < 1:
+			self.emit_signal("zero_health")
